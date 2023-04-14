4a029ff4ec1d575b7ad1a6f6eba491dcbc02df55
Olexandr Vaidych <10lexandr.vaidych@gmail.com>
2023-04-14 10:32:18 +0300

    Calculated convolution2

--- main.py
+++ main.py
@@ -11,24 +11,27 @@ def main():
 
     criterion1 = Criterion(table_data, 1)
     criteria1 = criterion1.get_criteria()
-    print(criteria1)
-
+    #print(criteria1)
     
     criterion2 = Criterion(table_data, 2)
     criteria2 = criterion2.get_criteria()
-    #print(criteria2)
+    print(criteria2)
     
     normalized_weight_coefficient1 = NormalizedWeightCoefficient(criteria1)
     normalized_weight_coefficients1 = normalized_weight_coefficient1.calc_norm_weight_coeffs()
-    print(normalized_weight_coefficients1)
+    #print(normalized_weight_coefficients1)
 
     normalized_weight_coefficient2 = NormalizedWeightCoefficient(criteria2)
     normalized_weight_coefficients2 = normalized_weight_coefficient2.calc_norm_weight_coeffs()
-    #print(normalized_weight_coefficients2)
+    print(normalized_weight_coefficients2)
 
     convolution1 = Convolution(criteria1, normalized_weight_coefficients1)
     average_convolution1 = convolution1.calc_average_convolution()
-    print(average_convolution1)
+    #print(average_convolution1)
+
+    convolution2 = Convolution(criteria2, normalized_weight_coefficients2)
+    average_convolution2 = convolution2.calc_average_convolution()
+    print(average_convolution2)
 
     root.mainloop()
 
