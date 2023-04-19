e02d2d2cd58526181a22c0fb9e236db2f0270609
Olexandr Vaidych <10lexandr.vaidych@gmail.com>
2023-04-19 11:42:11 +0300

    Prints were deleted

--- alternative/alternative_gui.py
+++ alternative/alternative_gui.py
@@ -12,51 +12,39 @@ def get_best_alternative(alternative_window):
 
     criterion1 = Criterion(table_data, 1)
     criteria1 = criterion1.get_criteria()
-    #print(criteria1)
     
     criterion2 = Criterion(table_data, 2)
     criteria2 = criterion2.get_criteria()
-    #print(criteria2)
 
     criterion3 = Criterion(table_data, 3)
     criteria3 = criterion3.get_criteria()
-    #print(criteria3)
 
     criterion4 = Criterion(table_data, 4)
     criteria4 = criterion4.get_criteria()
-    #print(criteria4)
     
     normalized_weight_coefficient1 = NormalizedWeightCoefficient(criteria1)
     normalized_weight_coefficients1 = normalized_weight_coefficient1.calc_norm_weight_coeffs()
-    #print(normalized_weight_coefficients1)
 
     normalized_weight_coefficient2 = NormalizedWeightCoefficient(criteria2)
     normalized_weight_coefficients2 = normalized_weight_coefficient2.calc_norm_weight_coeffs()
-    #print(normalized_weight_coefficients2)
 
     normalized_weight_coefficient3 = NormalizedWeightCoefficient(criteria3)
     normalized_weight_coefficients3 = normalized_weight_coefficient3.calc_norm_weight_coeffs()
-    #print(normalized_weight_coefficients3)
 
     normalized_weight_coefficient4 = NormalizedWeightCoefficient(criteria4)
     normalized_weight_coefficients4 = normalized_weight_coefficient4.calc_norm_weight_coeffs()
-    #print(normalized_weight_coefficients4)
 
     convolution1 = Convolution(criteria1, normalized_weight_coefficients1)
     average_convolution1 = convolution1.calc_average_convolution()
-    #print(average_convolution1)
 
     convolution2 = Convolution(criteria2, normalized_weight_coefficients2)
     average_convolution2 = convolution2.calc_average_convolution()
-    #print(average_convolution2)
 
     convolution3 = Convolution(criteria3, normalized_weight_coefficients3)
     average_convolution3 = convolution3.calc_average_convolution()
-    #print(average_convolution3)
 
     convolution4 = Convolution(criteria4, normalized_weight_coefficients4)
     average_convolution4 = convolution4.calc_average_convolution()
-    #print(average_convolution4)
 
     convolutions = [average_convolution1, average_convolution2, average_convolution3, average_convolution4]
     average_convolutions = {
@@ -70,7 +58,6 @@ def get_best_alternative(alternative_window):
 
     alternative = Alternative(max_convolution, average_convolutions)
     best_alternative = alternative.select_best_alternative()
-    #print(best_alternative)
 
     best_alternative_l = Label(alternative_window, text=best_alternative)
     best_alternative_l.grid(row=12, column=2)
