67caad36d14ab0dbdf1b34cd3144d639c431de59
Olexandr Vaidych <10lexandr.vaidych@gmail.com>
2023-04-21 10:48:56 +0300

    Automated method gui was added

---
+++ automated_method_gui.py
@@ -0,0 +1,21 @@
+from tkinter import Tk
+
+
+def authomated_method_gui():
+    authomated_method_window = Tk()
+    authomated_method_window.geometry("700x265")
+    """
+    table = Table(alternative_window)
+    empty_l = Label(alternative_window)
+    
+    get_best_alternative_b = Button(
+       alternative_window, 
+       text="The best alternative to determine", 
+       command=lambda: get_best_alternative(alternative_window)
+       )
+    
+    empty_l.grid(row=10, column=2)
+    get_best_alternative_b.grid(row=11, column=2)
+    """
+    
+    authomated_method_window.mainloop()
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from tkinter import Button, Tk
 from alternative.alternative_gui import alternative_gui
+from automated_method_gui import authomated_method_gui
 
 
 def main():
     root = Tk()
-    root.geometry("100x50")
+    root.geometry("250x50")
 
     alternative_b = Button(
        root, 
@@ -12,7 +13,15 @@ def main():
        command= lambda: alternative_gui()
        )
     
+    automated_method_b = Button(
+       root, 
+       text="Automated method",
+       command= lambda: authomated_method_gui()
+       )
+    
     alternative_b.grid(row=0, column=0)
+    automated_method_b.grid(row=0, column=1)
+    
     root.mainloop()
 
 if __name__== "__main__":
