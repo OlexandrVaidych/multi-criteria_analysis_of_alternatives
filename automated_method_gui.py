b9e94fd4b5e037ce9ae0598daa9d08a9a79cf8e2
Olexandr Vaidych <10lexandr.vaidych@gmail.com>
2023-04-26 12:14:00 +0300

    Prints deleted

--- automated_method_gui.py
+++ automated_method_gui.py
@@ -12,70 +12,53 @@ from table import Table
 def authomated_method(window, alternatives, pleasure_point):
     criterion1 = Criterion(table_data2, 1)
     criteria1 = criterion1.get_criteria() 
-    #print(criteria1)
 
     criterion2 = Criterion(table_data2, 2)
     criteria2 = criterion2.get_criteria() 
-    #print(criteria2)
 
     criterion3 = Criterion(table_data2, 3)
     criteria3 = criterion3.get_criteria() 
-    #print(criteria3)
 
     criterion4 = Criterion(table_data2, 4)
     criteria4 = criterion4.get_criteria() 
-    #print(criteria4)
 
     normalized_weight_coefficient1 = NormalizedWeightCoefficient(criteria1)
     normalized_weight_coefficients1 = normalized_weight_coefficient1.calc_norm_weight_coeffs()
-    #print(normalized_weight_coefficients1)
 
     normalized_weight_coefficient2 = NormalizedWeightCoefficient(criteria2)
     normalized_weight_coefficients2 = normalized_weight_coefficient2.calc_norm_weight_coeffs()
-    #print(normalized_weight_coefficients2)
 
     normalized_weight_coefficient3 = NormalizedWeightCoefficient(criteria3)
     normalized_weight_coefficients3 = normalized_weight_coefficient3.calc_norm_weight_coeffs()
-    #print(normalized_weight_coefficients3)
 
     normalized_weight_coefficient4 = NormalizedWeightCoefficient(criteria4)
     normalized_weight_coefficients4 = normalized_weight_coefficient4.calc_norm_weight_coeffs()
-    #print(normalized_weight_coefficients4)
 
     matrix_column1 = MatrixColumn(criteria1, pleasure_point)
     matrix_elements1 = matrix_column1.calc_matrix_column()
-    #print(matrix_elements1)
 
     matrix_column2 = MatrixColumn(criteria2, pleasure_point)
     matrix_elements2 = matrix_column2.calc_matrix_column()
-    #print(matrix_elements2)
 
     matrix_column3 = MatrixColumn(criteria3, pleasure_point)
     matrix_elements3 = matrix_column3.calc_matrix_column()
-    #print(matrix_elements3)
 
     matrix_column4 = MatrixColumn(criteria4, pleasure_point)
     matrix_elements4 = matrix_column4.calc_matrix_column()
-    #print(matrix_elements4)
 
     convolution1 = Convolution(matrix_elements1, normalized_weight_coefficients1)
     average_convolution1 = convolution1.calc_average_convolution()
-    #print(average_convolution1)
 
     convolution2 = Convolution(matrix_elements2, normalized_weight_coefficients2)
     average_convolution2 = convolution2.calc_average_convolution()
-    #print(average_convolution2)
 
     convolution3 = Convolution(matrix_elements3, normalized_weight_coefficients3)
     average_convolution3 = convolution3.calc_average_convolution()
-    #print(average_convolution3)
 
     convolution4 = Convolution(matrix_elements4, normalized_weight_coefficients4)
     average_convolution4 = convolution4.calc_average_convolution()
-    #print(average_convolution4)
 
     convolutions = [average_convolution1, average_convolution2, average_convolution3, average_convolution4]
-    #print(convolutions)
 
     average_convolutions = {
        alternatives[0]: convolutions[0],
@@ -86,7 +69,6 @@ def authomated_method(window, alternatives, pleasure_point):
     
     alternative = Alternative(convolutions, average_convolutions)
     best_alternative = alternative.select_best_alternative()
-    #print(best_alternative)
 
     best_alternative_l = Label(window, text=best_alternative)
     best_alternative_l.grid(row=7, column=2)
@@ -107,7 +89,6 @@ def authomated_method_gui():
    
     num = Number(pleasure_point_e)
     pleasure_point = num.get_nums()
-    #print(pleasure_point)
 
     get_best_alternative_b = Button(
        authomated_method_window, 
